f9162900c888496d18ac0b3192fc63ee24a7e06b
Thiago <thiagos3600@gmail.com>
2022-04-12 20:06:02 -0300

    Correção de inicialização

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -4,7 +4,7 @@ import View.Janela;
 
 public class Main {
     public static void main(String[] args) {
-         /*new Janela(9*55, 9*58-2, "Xadrez");*/
+         new Janela(9*55, 9*58-2, "Xadrez");
         
         
     }
